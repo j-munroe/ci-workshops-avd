d1ceecbd5af02f2608d3de3b89c2cc106e583b8d
Jim Munroe <jmunroe@gmail.com>
2023-10-18 15:11:21 +0000

    Final comment of bootcamp

--- start_me_up.sh
+++ start_me_up.sh
@@ -5,11 +5,15 @@ git config --global user.email "jmunroe@gmail.com"
 pip3 config set global.disable-pip-version-check true
 pip3 install "ansible-core<2.15.0"
 #
+echo "Install AVD Ansible Galaxy Collection: "
 ansible-galaxy collection install -r requirements.yml
-#
+
+echo "Set ARISTA_AVD_DIR Variable: "
 export ARISTA_AVD_DIR=$(ansible-galaxy collection list arista.avd --format yaml | head -1 | cut -d: -f1)
-#
+
+echo "Install AVD python requirements: "
 pip3 install -r ${ARISTA_AVD_DIR}/arista/avd/requirements.txt
 #
+echo "Spit out the lab pass phrase: "
 export LABPASSPHRASE=`cat /home/coder/.config/code-server/config.yaml| grep "password:" | awk '{print $2}'`
 echo $LABPASSPHRASE
